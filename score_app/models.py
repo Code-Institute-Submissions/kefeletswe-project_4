d4330615d96e1a0e390a291e044b28210fc8cc86
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-06-08 19:44:35 +0000

    in models.py line 4 to 13 add models with their field types and the migrate

--- score_app/models.py
+++ score_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Score_app(models.Model):
+participant_number = models.PositiveIntegerField()
+first_name = models.CharField(max_length=50)
+last_name = models.CharField(max_length=50)
+email = models.EmailField(max_length=100)
+phone_number = models.CharField(max_length=50)
+age = models.PositiveIntegerField(null=True, blank=True)
+
+def __str__(self):
+  return f'Score_app: {self.first_name} {self.last_name}'
