a0c289ec6fd6e4a973e70671b71466e8a708daa2
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-06-08 20:31:42 +0000

    create a static folder , add a css folder in it then download boostrap and drag into css folder,in index.html file line 1 add commnad load static and add css and bootsrap links in the head line 8-9

--- score_app/views.py
+++ score_app/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+# diplay the list of people who will participate in the marathon
+from .models import Participants
 
 # Create your views here.
 def index(request):
-    return render(request, 'score_app/index.html')
+    return render(request, 'score_app/index.html',{
+    'score_app': Participants.objects.all()
+    }) 
