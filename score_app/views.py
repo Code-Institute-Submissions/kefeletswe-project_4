81347284192bbc5c8ce8eec00e66994a89a43873
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-06-08 19:21:52 +0000

    create function based views, then createan html file inside templates, create new file inside the score_app directory and import views, then define urlpatterns and add 3 paths inside it

--- score_app/views.py
+++ score_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'records/index.html')
+    return render(request, 'score_app/index.html')
