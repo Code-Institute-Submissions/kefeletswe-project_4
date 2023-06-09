240803b4d06523c4dce2f1ccd07d1e1b447da37e
kefeletswe <kefeletswenonhlanhla@gmail.com>
2023-06-09 01:51:21 +0000

    add endfor tag in line 77 and create superuser and add participants database in admin

--- score_app/models.py
+++ score_app/models.py
@@ -6,7 +6,7 @@ class Participants(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email = models.EmailField(max_length=100)
-    phone_number = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=8)
     age = models.PositiveIntegerField(null=True, blank=True)
 
 def __str__(self):
